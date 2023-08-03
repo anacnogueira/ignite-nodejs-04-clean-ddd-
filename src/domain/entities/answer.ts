c35260fb992780ba43fdb6559e679d3df9c1dae6
Ana Claudia <anacnogueira@gmail.com>
2023-08-03 20:46:25 -0300

    Mapeando Propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,13 @@
 import { randomUUID } from "node:crypto";
 import { Entity } from "../../core/entities/entities";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface AnswerProps {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date;
+  updateAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {}
