f6d7215d723707b1fdcb0aea3d6eab35a3feedec
Ana Claudia <anacnogueira@gmail.com>
2023-08-05 20:44:11 -0300

    Getters e Setters das entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,8 @@
-import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entities";
 import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Optional } from "../../core/entities/types/optional";
+import dayjs from "dayjs";
 
 interface QuestionProps {
   authorId: UniqueEntityID;
@@ -11,17 +11,74 @@ interface QuestionProps {
   content: string;
   slug: Slug;
   createdAt: Date;
-  updateAt?: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromtext(title);
+    this.touch();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, "days") <= 3;
+  }
+
   static create(
-    props: Optional<QuestionProps, "createdAt">,
+    props: Optional<QuestionProps, "createdAt" | "slug">,
     id?: UniqueEntityID
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromtext(props.title),
         createdAt: new Date(),
       },
       id
