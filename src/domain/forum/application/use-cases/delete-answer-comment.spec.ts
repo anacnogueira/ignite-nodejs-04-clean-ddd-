36602e4f1c1521388159fd815b46b3cd591582a1
Ana Claudia <anacnogueira@gmail.com>
2023-08-20 15:15:36 -0300

    Criando erros genéricos

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-an
 import { DeleteAnswerCommentUseCase } from "./delete-answer-comment";
 import { makeAnswerComment } from "test/factories/make-answer-comment";
 import { UniqueEntityID } from "@/core/types/entities/unique-entity-id";
+import { NotAllowedError } from "./errors/not-allowed-error";
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
 let sut: DeleteAnswerCommentUseCase;
@@ -12,7 +13,7 @@ describe("Delete Answer Comment", () => {
     sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
   });
 
-  it.skip("should be able to delete a answer comment", async () => {
+  it("should be able to delete a answer comment", async () => {
     const answerComment = makeAnswerComment();
 
     await inMemoryAnswerCommentsRepository.create(answerComment);
@@ -25,18 +26,18 @@ describe("Delete Answer Comment", () => {
     expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
   });
 
-  it.skip("should not be able to delete another user answer comment", async () => {
+  it("should not be able to delete another user answer comment", async () => {
     const answerComment = makeAnswerComment({
       authorId: new UniqueEntityID("author-1"),
     });
 
     await inMemoryAnswerCommentsRepository.create(answerComment);
 
-    expect(() => {
-      return sut.execute({
-        answerCommentId: answerComment.id.toString(),
-        authorId: "author-2",
-      });
-    }).rejects.toBeInstanceOf(Error);
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: "author-2",
+    });
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
